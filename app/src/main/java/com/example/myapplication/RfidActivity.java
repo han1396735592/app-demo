22e1f268c813cf2ddc1695f46fb885b77919a5c1
han1396735592 <1396735592@qq.com>
2019-12-30 17:09:45 +0800

    增加了模拟识别提示

--- app/src/main/java/com/example/myapplication/RfidActivity.java
+++ app/src/main/java/com/example/myapplication/RfidActivity.java
@@ -10,21 +10,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class RfidActivity extends AppCompatActivity {
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        try {
-            Thread.sleep(3000);
-            Toast.makeText(this, "模拟识别出结果", Toast.LENGTH_SHORT).show();
-            setContentView(R.layout.recognition_result);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +24,10 @@ public class RfidActivity extends AppCompatActivity {
         Button button = findViewById(R.id.cancel);
         button.setOnClickListener(view -> {
             RfidActivity.this.finish();
+        });
+        ImageView image = findViewById(R.id.img);
+        image.setOnClickListener(v -> {
+            setContentView(R.layout.recognition_result);
         });
     }
 }
