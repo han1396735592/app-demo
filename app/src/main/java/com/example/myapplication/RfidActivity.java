b9f80948b34a7b195cba4fc4140e86390def9bf6
han1396735592 <1396735592@qq.com>
2019-12-30 16:36:07 +0800

    增加了RFID识别结果页面

--- app/src/main/java/com/example/myapplication/RfidActivity.java
+++ app/src/main/java/com/example/myapplication/RfidActivity.java
@@ -10,8 +10,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RfidActivity extends AppCompatActivity {
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        try {
+            Thread.sleep(3000);
+            Toast.makeText(this, "模拟识别出结果", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.recognition_result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +32,6 @@ public class RfidActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rfid);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         Button button = findViewById(R.id.cancel);
         button.setOnClickListener(view -> {
             RfidActivity.this.finish();
